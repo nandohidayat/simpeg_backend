6cdb6f47d631accb2e95c4875bf7f68807c428ce
nandohidayat <mnandohidayat@gmail.com>
2020-03-28 19:45:18 +0700

    fix error on absen bcs of groub by a.datetime, shifts.mulai

--- app/Http/Controllers/API/AbsenController.php
+++ app/Http/Controllers/API/AbsenController.php
@@ -117,7 +117,7 @@ class AbsenController extends Controller
                 DB::raw("(case when cast(a.datetime as time) < (cast(shifts.mulai as time) + interval '5 minutes') OR (cast(shifts.mulai as time) = time '00:00') then 'Tidak Terlambat' else 'Terlambat' end) as keterangan"),
                 DB::raw("(case when cast(a.datetime as time) < (cast(shifts.mulai as time) + interval '5 minutes') OR (cast(shifts.mulai as time) = time '00:00') then (SELECT ph.pendapatan FROM pendapatan_harians as ph WHERE ph.tgl <= schedules.tgl ORDER BY ph.tgl DESC LIMIT 1) else 0 end) as pendapatan"),
             )
-            ->groupBy('schedules.id_schedule', 'shifts.kode', 'keterangan', 'pendapatan')
+            ->groupBy('schedules.id_schedule', 'shifts.kode', 'keterangan', 'pendapatan', 'a.datetime', 'shifts.mulai')
             ->get();
 
         $sum = 0;
