9de4c86dae4506cc612e3342769d9379c61ee171
nandohidayat <mnandohidayat@gmail.com>
2021-07-09 15:07:46 +0700

    feat: done table pendapatan buat gaji sama premi

--- app/Http/Controllers/API/PendapatanController.php
+++ app/Http/Controllers/API/PendapatanController.php
@@ -23,13 +23,8 @@ class PendapatanController extends Controller
         $data = DB::table('pendapatan_lists as pl')
             ->where('pl.id_pendapatan_list', $list)
             ->join('pendapatan_profils as pp', 'pp.id_pendapatan_profil', '=', 'pl.id_pendapatan_profil')
-            ->select('pp.column', 'pl.month', 'pl.distribution as distri', 'pl.id_pendapatan_profil as profil', 'pl.locked', 'pl.edit')
+            ->select('pp.column', 'pl.month', 'pl.distribution as distri', 'pl.id_pendapatan_profil as profil', 'pl.locked')
             ->first();
-
-        $edit = $data->edit;
-        if ($data->edit) {
-            $edit = DB::table('f_data_pegawai')->where('id_pegawai', $data->edit)->select('id_pegawai', 'nm_pegawai')->first();
-        }
 
         $column = json_decode($data->column) ?? [];
         $firstDay = Carbon::createFromFormat('Y-m-d', $data->month)->firstOfMonth();
@@ -47,12 +42,13 @@ class PendapatanController extends Controller
             ->whereRaw('coalesce(ld.keluar, ?) <= ?', [$lastDay, $lastDay])
             ->select('fdp.id_pegawai', 'fdp.nik_pegawai', 'fdp.nm_pegawai', DB::raw('json_agg(fd.nm_dept) as nm_dept'));
 
+
         $c_array = ['fdp.id_pegawai', 'fdp.nik_pegawai', 'fdp.nm_pegawai'];
         foreach ($column as $value) {
             $c = strtolower($value->field);
             if (in_array($c, ['nik_pegawai', 'nm_pegawai', 'nm_dept'])) {
                 continue;
-            } else if (in_array($c, ['premi3v', 'premi4v1', 'premi4v2', 'premi4'])) {
+            } else if (in_array($c, ['premi3v', 'premi4v1', 'premi4v2', 'premi4', 'premi6p', 'premi7p'])) {
                 array_push($c_array, $c);
                 continue;
             } else {
@@ -110,7 +106,7 @@ class PendapatanController extends Controller
                             'id_pegawai',
                             DB::raw('SUM(CASE WHEN makan > 0 THEN 1 ELSE 0 END) as premi3v'),
                             DB::raw('SUM(makan) as premi3'),
-                            DB::raw('SUM(CASE WHEN harian = 0 THEN 1 ELSE 0 END) as premi4v1'),
+                            DB::raw('SUM(CASE WHEN harian = 0 AND cast(shift as time) <> time \'00:00\' THEN 1 ELSE 0 END) as premi4v1'),
                             DB::raw('SUM(CASE WHEN harian > 0 THEN 1 ELSE 0 END) as premi4v2'),
                             DB::raw('SUM(harian) as premi4')
                         )
@@ -125,7 +121,7 @@ class PendapatanController extends Controller
                     $query->leftJoin('pendapatan_makans as t_premi3p', function ($join) use ($lastDay) {
                         $join->on('t_premi3p.tgl', '=', DB::raw('(SELECT MAX(pm_t.tgl) FROM pendapatan_makans as pm_t WHERE pm_t.tgl <= \'' . $lastDay . '\')'));
                     });
-                    $query->addSelect('t_premi3p.pendapatan as premi3p');
+                    $query->addSelect('t_premi3p.pendapatan as premi3p', 't_premi3p.pendapatan as premi6p');
                     continue;
                 }
 
@@ -133,50 +129,77 @@ class PendapatanController extends Controller
                     $query->leftJoin('pendapatan_harians as t_premi4p', function ($join) use ($lastDay) {
                         $join->on('t_premi4p.tgl', '=', DB::raw('(SELECT MAX(ph_t.tgl) FROM pendapatan_harians as ph_t WHERE ph_t.tgl <= \'' . $lastDay . '\')'));
                     });
-                    $query->addSelect('t_premi4p.pendapatan as premi4p');
+                    $query->addSelect('t_premi4p.pendapatan as premi4p', 't_premi4p.pendapatan as premi7p');
                     continue;
                 }
 
                 if ($c === 'pjk1') {
-                    $query->leftJoin(DB::raw("(SELECT pen.label, pen.value, pl.distribution, pen.id_pegawai, row_number() over (order by pl.distribution desc) as rn from pendapatans as pen LEFT JOIN pendapatan_lists as pl ON pl.id_pendapatan_list = pen.id_pendapatan_list) t_pjk1"), function ($query) use ($first, $firstOfYear) {
+                    $lastPjk7 = DB::table('pendapatans as pen')
+                        ->leftJoin('pendapatan_lists as pl', 'pl.id_pendapatan_list', '=', 'pen.id_pendapatan_list')
+                        ->select('pen.value', 'id_pegawai', DB::raw('row_number() over (partition by pen.id_pegawai order by pl.distribution desc) as rn'))
+                        ->where('label', 'pjk7')
+                        ->where('distribution', '<', $first)
+                        ->where('distribution', '>=', $firstOfYear);
+
+                    $query->leftJoinSub($lastPjk7, 't_pjk1', function ($query) {
                         $query->where('t_pjk1.rn', 1);
-                        $query->where('t_pjk1.label', 'pjk7');
-                        $query->where('t_pjk1.distribution', '<', $first);
-                        $query->where('t_pjk1.distribution', '>=', $firstOfYear);
                         $query->on('t_pjk1.id_pegawai', '=', 'fdp.id_pegawai');
                     });
+
                     $query->addSelect(DB::raw('cast(t_pjk1.value as DECIMAL) as pjk1'));
                     continue;
                 }
                 if ($c === 'pjk2') {
-                    $query->addSelect(DB::raw("(SELECT SUM(cast(pen.value AS DECIMAL)) FROM pendapatans as pen LEFT JOIN pendapatan_lists as pl ON pl.id_pendapatan_list = pen.id_pendapatan_list WHERE pen.label = 'jmlhpremi' AND pl.distribution >= '$first' AND pl.distribution <= '$last' AND pen.id_pegawai = fdp.id_pegawai) as pjk2"));
+                    $query->addSelect(DB::raw("(SELECT SUM(cast(pen.value AS DECIMAL)) FROM pendapatans as pen LEFT JOIN pendapatan_lists as pl ON pl.id_pendapatan_list = pen.id_pendapatan_list WHERE pen.label = 'jmlhpremi' AND pl.distribution >= '$first' AND pl.distribution < '$last' AND pen.id_pegawai = fdp.id_pegawai) as pjk2"));
                     continue;
                 }
                 if ($c === 'pjk3') {
-                    $query->addSelect(DB::raw("(SELECT SUM(cast(pen.value AS DECIMAL)) FROM pendapatans as pen LEFT JOIN pendapatan_lists as pl ON pl.id_pendapatan_list = pen.id_pendapatan_list WHERE pen.label = 'jmlhgaji' AND pl.distribution >= '$first' AND pl.distribution <= '$last' AND pen.id_pegawai = fdp.id_pegawai) as pjk3"));
+                    $query->addSelect(DB::raw("(SELECT SUM(cast(pen.value AS DECIMAL)) FROM pendapatans as pen LEFT JOIN pendapatan_lists as pl ON pl.id_pendapatan_list = pen.id_pendapatan_list WHERE pen.label = 'jmlhgaji' AND pl.distribution >= '$first' AND pl.distribution < '$last' AND pen.id_pegawai = fdp.id_pegawai) as pjk3"));
                     continue;
                 }
                 if ($c === 'pjk4') {
-                    $query->addSelect(DB::raw("(SELECT SUM(cast(pen.value AS DECIMAL)) FROM pendapatans as pen LEFT JOIN pendapatan_lists as pl ON pl.id_pendapatan_list = pen.id_pendapatan_list WHERE pen.label = 'jmlhinsentiv' AND pl.distribution >= '$first' AND pl.distribution <= '$last' AND pen.id_pegawai = fdp.id_pegawai) as pjk4"));
+                    $query->addSelect(DB::raw("(SELECT SUM(cast(pen.value AS DECIMAL)) FROM pendapatans as pen LEFT JOIN pendapatan_lists as pl ON pl.id_pendapatan_list = pen.id_pendapatan_list WHERE pen.label = 'jmlhinsentiv' AND pl.distribution >= '$first' AND pl.distribution < '$last' AND pen.id_pegawai = fdp.id_pegawai) as pjk4"));
                     continue;
                 }
                 if ($c === 'pjk5') {
-                    $query->addSelect(DB::raw("(SELECT SUM(cast(pen.value AS DECIMAL)) FROM pendapatans as pen LEFT JOIN pendapatan_lists as pl ON pl.id_pendapatan_list = pen.id_pendapatan_list WHERE pen.label = 'jmlhgajike' AND pl.distribution >= '$first' AND pl.distribution <= '$last' AND pen.id_pegawai = fdp.id_pegawai) as pjk5"));
+                    $query->addSelect(DB::raw("(SELECT SUM(cast(pen.value AS DECIMAL)) FROM pendapatans as pen LEFT JOIN pendapatan_lists as pl ON pl.id_pendapatan_list = pen.id_pendapatan_list WHERE pen.label = 'jmlhgajike' AND pl.distribution >= '$first' AND pl.distribution < '$last' AND pen.id_pegawai = fdp.id_pegawai) as pjk5"));
                     continue;
                 }
                 if ($c === 'pjk6') {
-                    $query->addSelect(DB::raw("(SELECT (SUM(cast(pen.value AS DECIMAL)) * -1) FROM pendapatans as pen LEFT JOIN pendapatan_lists as pl ON pl.id_pendapatan_list = pen.id_pendapatan_list WHERE pen.label = 'premi3' AND pl.distribution >= '$first' AND pl.distribution <= '$last' AND pen.id_pegawai = fdp.id_pegawai) as pjk6"));
+                    $query->addSelect(DB::raw("(SELECT (SUM(cast(pen.value AS DECIMAL)) * -1) FROM pendapatans as pen LEFT JOIN pendapatan_lists as pl ON pl.id_pendapatan_list = pen.id_pendapatan_list WHERE (pen.label = 'premi3' OR pen.label = 'premi6') AND pl.distribution > '$first' AND pl.distribution < '$last' AND pen.id_pegawai = fdp.id_pegawai) as pjk6"));
+                    continue;
+                }
+
+                if ($c === 'pjk8') {
+                    $lastPjk8 = DB::table('pendapatans as pen')
+                        ->leftJoin('pendapatan_lists as pl', 'pl.id_pendapatan_list', '=', 'pen.id_pendapatan_list')
+                        ->select('pen.value', 'id_pegawai', DB::raw('row_number() over (partition by pen.id_pegawai order by pl.distribution desc) as rn'))
+                        ->where('label', 'pjk8')
+                        ->where('distribution', '<', $last)
+                        ->where('distribution', '>=', $firstOfYear);
+
+                    $query->leftJoinSub($lastPjk8, 't_pjk8', function ($query) {
+                        $query->where('t_pjk8.rn', 1);
+                        $query->on('t_pjk8.id_pegawai', '=', 'fdp.id_pegawai');
+                    });
+
+                    $query->addSelect(DB::raw('cast(t_pjk8.value as DECIMAL) as pjk8'));
                     continue;
                 }
 
                 if ($c === 'pjk13') {
-                    $query->leftJoin(DB::raw("(SELECT pen.label, pen.value, pl.distribution, pen.id_pegawai, row_number() over (order by pl.distribution desc) as rn from pendapatans as pen LEFT JOIN pendapatan_lists as pl ON pl.id_pendapatan_list = pen.id_pendapatan_list) t_pjk13"), function ($query) use ($first, $firstOfYear) {
+                    $lastPjk12 = DB::table('pendapatans as pen')
+                        ->leftJoin('pendapatan_lists as pl', 'pl.id_pendapatan_list', '=', 'pen.id_pendapatan_list')
+                        ->select('pen.value', 'id_pegawai', DB::raw('row_number() over (partition by pen.id_pegawai order by pl.distribution desc) as rn'))
+                        ->where('label', 'pjk12')
+                        ->where('distribution', '<', $first)
+                        ->where('distribution', '>=', $firstOfYear);
+
+                    $query->leftJoinSub($lastPjk12, 't_pjk13', function ($query) {
                         $query->where('t_pjk13.rn', 1);
-                        $query->where('t_pjk13.label', 'pjk12');
-                        $query->where('t_pjk13.distribution', '<', $first);
-                        $query->where('t_pjk13.distribution', '>=', $firstOfYear);
                         $query->on('t_pjk13.id_pegawai', '=', 'fdp.id_pegawai');
                     });
+
                     $query->addSelect(DB::raw('cast(t_pjk13.value as DECIMAL) as pjk13'));
                     continue;
                 }
@@ -206,8 +229,27 @@ class PendapatanController extends Controller
         if (!$locked) {
             foreach ($data as $key => $value) {
                 $pen = $data[$key];
+
+                if ((int)$profil === 5) {
+                    $pen->premi6 = $pen->premi6v * $pen->premi6p;
+                    $pen->premi7 = $pen->premi7v2 * $pen->premi7p;
+
+                    $pen->jmlhpremi = $pen->premi1 + $pen->premi2 + $pen->premi3 + $pen->premi4 + $pen->premi6 + $pen->premi7;
+                    $pen->premi5 = ($pen->jmlhpremi - ($pen->premi3 + $pen->premi6)) * (2.5 / 100);
+                    $pen->diterima = $pen->jmlhpremi - $pen->premi5;
+
+                    $pen->pjk2 = $pen->pjk2 + $pen->jmlhpremi;
+                    $pen->pjk6 = $pen->pjk6 - ($pen->premi3 + $pen->premi6);
+
+                    $pen->penyerahan = $pen->diterima + $pen->jmlhpotg;
+                }
+
+                if ((int)$profil === 1) {
+                    $pen->pjk3 = $pen->pjk3 + $pen->jmlhgaji;
+                    $pen->pjk8 = $pen->pjk8 - ($pen->pjk3 * 0.05);
+                }
+
                 $pen->pjk7 = $pen->pjk1 + $pen->pjk2 + $pen->pjk3 + $pen->pjk4 + $pen->pjk5 + $pen->pjk6;
-                $pen->pjk8 = - ($pen->pjk3 * 0.05);
                 $pen->pjk9 = - ($pen->pjk7 * 0.05);
                 $pen->pjk10 = - ($pen->ptkp * $last->month);
                 $pen->pjk11 = max($pen->pjk7 + $pen->pjk8 + $pen->pjk9 + $pen->pjk10, 0);
@@ -215,7 +257,7 @@ class PendapatanController extends Controller
                 $pen->pjk14 = - ($pen->pjk12);
                 $pen->pjk15 = $pen->pjk12 - $pen->pjk13;
 
-                if (property_exists($pen, 'jmlhgaji')) {
+                if ((int)$profil === 1) {
                     $pen->pot1 = - ($pen->pjk15);
                     $pen->pot10 = $pen->pjk15;
 
@@ -240,7 +282,7 @@ class PendapatanController extends Controller
             }
         }
 
-        return response()->json(["status" => "success", "data" => ['pendapatan' => $data, 'profil' => $profil, 'date' => $last, 'edit' => $edit]], 200)->setEncodingOptions(JSON_NUMERIC_CHECK);
+        return response()->json(["status" => "success", "data" => ['pendapatan' => $data, 'profil' => $profil, 'date' => $last]], 200)->setEncodingOptions(JSON_NUMERIC_CHECK);
     }
 
     /**
@@ -273,12 +315,13 @@ class PendapatanController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $list = (int) $request->list;
+        $list = (int) $id;
+        $tipe = (int) $request->tipe;
 
         $data = DB::table('pendapatan_lists as pl')
             ->where('pl.id_pendapatan_list', $list)
             ->join('pendapatan_profils as pp', 'pp.id_pendapatan_profil', '=', 'pl.id_pendapatan_profil')
-            ->select('pp.column', 'pl.month', 'pl.distribution as distri', 'pl.id_pendapatan_profil as profil', 'pl.locked')
+            ->select('pp.column')
             ->first();
 
         $column = json_decode($data->column) ?? [];
@@ -287,7 +330,7 @@ class PendapatanController extends Controller
 
         foreach ($request->pendapatan as $p) {
             foreach ($column as $value) {
-                if ($value->field === 'nik_pegawai' || $value->field === 'nm_pegawai' || $value->field === 'nm_dept') {
+                if ((int)$value->author !== $tipe || $value->field === 'nik_pegawai' || $value->field === 'nm_pegawai' || $value->field === 'nm_dept') {
                     continue;
                 }
 
